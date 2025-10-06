ed412dd3c9fefab5dadf12cf51f2ae4ac48c88ef
Asikur Rahman <mdasikur5893@gmail.com>
2025-10-06 13:39:46 +0600

    Search done

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,27 +1,45 @@
-import React from 'react';
-import useProducts from '../Hook/useProducts';
-import ProductsCard from './ProductsCard';
+import React, { useState } from "react";
+import useProducts from "../Hook/useProducts";
+import ProductsCard from "./ProductsCard";
 
 const Products = () => {
-    const {products} = useProducts()
-    // console.log(products)
-    return (
-        <div className='container'>
-             <div className='flex justify-between items-center my-4'>
-                <div>
-                <h1 className='font-bold'>All Products <span className='text-gray-500'> {products.length} products fund</span></h1>
-                </div>
-                <div>
-               <input type="search" placeholder="Serch" className="input" />
-                </div>
-            </div>
-           <div className='grid md:grid-cols-4 gap-5'>
-            {
-                products.map(product=><ProductsCard key={product.id} product={product}></ProductsCard>)
-            }
-           </div>
+  const { products } = useProducts();
+  const [search, setSearch] = useState("");
+  const trims = search.trim().toLocaleLowerCase();
+  const searchProducts = trims
+    ? products.filter((product) =>
+        product.name.toLocaleLowerCase().includes(trims)
+      )
+    : products;
+//   console.log(searchProducts);
+  return (
+    <div className="container">
+      <div className="flex justify-between items-center my-4">
+        <div>
+          <h1 className="font-bold">
+            All Products{" "}
+            <span className="text-gray-500">
+              {" "}
+              {searchProducts.length} products fund
+            </span>
+          </h1>
         </div>
-    );
+        <div>
+          <input
+            onChange={(e) => setSearch(e.target.value)}
+            type="search"
+            placeholder="Serch"
+            className="input"
+          />
+        </div>
+      </div>
+      <div className="grid md:grid-cols-4 gap-5">
+        {searchProducts.map((product) => (
+          <ProductsCard key={product.id} product={product}></ProductsCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Products;
