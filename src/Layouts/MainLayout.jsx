b409e93f31b645bef67b1753cb2ef6f11f275fcb
Asikur Rahman <mdasikur5893@gmail.com>
2025-10-06 10:17:22 +0600

    Nav & Footer Added

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import Navbar from '../Components/Navbar';
+import { Outlet } from 'react-router';
+import Footer from '../Components/Footer';
 
 const MainLayout = () => {
     return (
-        <div>
-            
+        <div className='flex flex-col min-h-screen'>
+            <Navbar></Navbar>
+            <div className='flex flex-1'>
+                 <Outlet></Outlet>
+            </div>
+            <Footer></Footer>
         </div>
     );
 };
--- src/Routing/Routing.jsx
+++ src/Routing/Routing.jsx
@@ -1,9 +1,26 @@
 import { createBrowserRouter } from "react-router";
 import Home from "../Pages/Home";
+import MainLayout from "../Layouts/MainLayout";
+import Products from "../Pages/Products";
+import WishList from "../Pages/WishList";
 
-export const router=createBrowserRouter([
-    {
-        path:'/',
-        element:<Home></Home>
-    }
-])
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children: [
+      {
+        index: true,
+        element: <Home></Home>,
+      },
+      {
+        path: "/products",
+        element: <Products></Products>,
+      },
+      {
+        path: "/wishList",
+        element: <WishList></WishList>,
+      },
+    ],
+  },
+]);
