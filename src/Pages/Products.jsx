5d0faf30b43f3ad195604b1b78a380bbc47e2349
Asikur Rahman <mdasikur5893@gmail.com>
2025-10-06 12:02:02 +0600

    See All Product Pages added

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import useProducts from '../Hook/useProducts';
+import ProductsCard from './ProductsCard';
 
 const Products = () => {
+    const {products} = useProducts()
+    // console.log(products)
     return (
-        <div>
-            <h1>hello iam porducts pages</h1>
+        <div className='container'>
+             <div className='flex justify-between items-center my-4'>
+                <div>
+                <h1 className='font-bold'>All Products <span className='text-gray-500'> {products.length} products fund</span></h1>
+                </div>
+                <div>
+               <input type="search" placeholder="Serch" className="input" />
+                </div>
+            </div>
+           <div className='grid md:grid-cols-4 gap-5'>
+            {
+                products.map(product=><ProductsCard key={product.id} product={product}></ProductsCard>)
+            }
+           </div>
         </div>
     );
 };
